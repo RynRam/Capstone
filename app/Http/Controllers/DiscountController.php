17938fedddc75b8bef001efaf38dd5c09bda05f0
RynRam <ryankenethramiso@gmail.com>
2019-03-09 03:08:07 +0800

    Checked Redirect

--- app/Http/Controllers/DiscountController.php
+++ app/Http/Controllers/DiscountController.php
@@ -52,7 +52,7 @@ class DiscountController extends Controller
         $discounts->discount = $request->discount;
         $discounts->save();
 
-        return view('admin/discount');
+        return view('admin.discount.index');
     }
 
     /**
