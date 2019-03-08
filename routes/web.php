8acbca01edf19017033cea19344c84cd8dad3447
RynRam <ryankenethramiso@gmail.com>
2019-03-08 18:46:30 +0800

    test URLs

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,11 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+
 */
+if (env('APP_ENV') === 'production') {
+    URL::forceSchema('https');
+}
 	//Customer Pages
 	Route::get('/', 'FrontController@getIndex');
 	Route::get('/basic-catering', 'FrontController@getCatering');
