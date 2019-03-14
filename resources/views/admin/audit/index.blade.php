35c7cee91e9d1f9b06711415a73d23f0d915728e
RynRam <ryankenethramiso@gmail.com>
2019-03-14 09:24:09 +0800

    audit fix responsive

--- resources/views/admin/audit/index.blade.php
+++ resources/views/admin/audit/index.blade.php
@@ -6,10 +6,8 @@
     <h1>Audits Trail</h1>
 
   </div>
-
   <div class="container-fluid">
     <hr>
-
     <div class="row-fluid">
       <div class="span12">
         <div class="widget-box">
@@ -17,24 +15,23 @@
             <h5>Data table</h5>
           </div>
           <div class="widget-content nopadding">
-            <div id="DataTables_Table_0_wrapper" class="dataTables_wrapper" role="grid"><div class=""><div id="DataTables_Table_0_length" class="dataTables_length"><label>Show <div class="select2-container" id="s2id_autogen1">    <a href="#" onclick="return false;" class="select2-choice">   <span>10</span><abbr class="select2-search-choice-close" style="display:none;"></abbr>   <div><b></b></div></a>    <div class="select2-drop select2-offscreen">   <div class="select2-search">       <input type="text" autocomplete="off" class="select2-input" tabindex="0">   </div>   <ul class="select2-results">   </ul></div></div><select size="1" name="DataTables_Table_0_length" aria-controls="DataTables_Table_0" style="display: none;"><option value="10" selected="selected">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div>
-            <table class="table table-bordered data-table dataTable" id="DataTables_Table_0">
-            <thead>
-                <tr role="row">
-                <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 244px;"><div class="DataTables_sort_wrapper">ID<span class="DataTables_sort_icon css_right ui-icon ui-icon-triangle-1-n"></span></div></th>
-                <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 373px;"><div class="DataTables_sort_wrapper">USER<span class="DataTables_sort_icon css_right ui-icon ui-icon-carat-2-n-s"></span></div></th>
-                <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 350px;"><div class="DataTables_sort_wrapper">EVENT<span class="DataTables_sort_icon css_right ui-icon ui-icon-carat-2-n-s"></span></div></th>
-                <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 244px;"><div class="DataTables_sort_wrapper">AUDIT TYPE<span class="DataTables_sort_icon css_right ui-icon ui-icon-triangle-1-n"></span></div></th>
-                <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 373px;"><div class="DataTables_sort_wrapper">OLD VALUES<span class="DataTables_sort_icon css_right ui-icon ui-icon-carat-2-n-s"></span></div></th>
-                <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 350px;"><div class="DataTables_sort_wrapper">NEW VALUES<span class="DataTables_sort_icon css_right ui-icon ui-icon-carat-2-n-s"></span></div></th>
-                <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 206px;"><div class="DataTables_sort_wrapper">DATE CREATED<span class="DataTables_sort_icon css_right ui-icon ui-icon-carat-2-n-s"></span></div></th>
-                <th class="ui-state-default" role="columnheader" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 206px;"><div class="DataTables_sort_wrapper">DATE UPDATED<span class="DataTables_sort_icon css_right ui-icon ui-icon-carat-2-n-s"></span></div></th>
+            <table class="table table-bordered data-table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>USER</th>
+                  <th>EVENT</th>
+                  <th>AUDIT  TYPE</th>
+                  <th>OLD VALUES</th>
+                  <th>NEW VALUES</th>
+                  <th>CREATED AT</th>
+                  <th>UPDATED AT</th>
                 </tr>
               </thead>
-              
-            <tbody role="alert" aria-live="polite" aria-relevant="all">
-            @foreach($audits as $audits)
-                <tr class="gradeU odd">
+              <tbody>
+                
+                @foreach($audits as $audits)
+                <tr class="">
                 <td class="  ">{{$audits->id}}</td>
                   <td class=" ">{{$audits->user}}</td>
                   <td class=" ">{{$audits->event}}</td>
@@ -45,13 +42,13 @@
                   <td class=" ">{{$audits->updated_at->diffForHumans()}}</td>
                 </tr>
                 @endforeach
-                </tbody></table><div class="fg-toolbar ui-toolbar ui-widget-header ui-corner-bl ui-corner-br ui-helper-clearfix"><div class="dataTables_filter" id="DataTables_Table_0_filter"><label>Search: <input type="text" aria-controls="DataTables_Table_0"></label></div><div class="dataTables_paginate fg-buttonset ui-buttonset fg-buttonset-multi ui-buttonset-multi paging_full_numbers" id="DataTables_Table_0_paginate"><a tabindex="0" class="first ui-corner-tl ui-corner-bl fg-button ui-button ui-state-default" id="DataTables_Table_0_first">First</a><a tabindex="0" class="previous fg-button ui-button ui-state-default" id="DataTables_Table_0_previous">Previous</a><span><a tabindex="0" class="fg-button ui-button ui-state-default">2</a><a tabindex="0" class="fg-button ui-button ui-state-default">3</a><a tabindex="0" class="fg-button ui-button ui-state-default ui-state-disabled">4</a><a tabindex="0" class="fg-button ui-button ui-state-default">5</a><a tabindex="0" class="fg-button ui-button ui-state-default">6</a></span><a tabindex="0" class="next fg-button ui-button ui-state-default" id="DataTables_Table_0_next">Next</a><a tabindex="0" class="last ui-corner-tr ui-corner-br fg-button ui-button ui-state-default" id="DataTables_Table_0_last">Last</a></div></div></div>
+                
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
     </div>
-
-
   </div>
 
 </div>
