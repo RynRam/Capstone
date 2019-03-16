005cfe32c40e74c5ad090b623b13d3cb348333a3
RynRam <ryankenethramiso@gmail.com>
2019-03-17 01:42:31 +0800

    Email

--- app/Http/Controllers/CustomerRegisterController.php
+++ app/Http/Controllers/CustomerRegisterController.php
@@ -51,7 +51,6 @@ class CustomerRegisterController extends Controller
     public function sendEmail($customer){
   
         Mail::to($customer["email"])->send(new Verifyemail($customer));
-        dd('Mail Sent');
        
     }
 
