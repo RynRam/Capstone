5caa3df1bd14da23c406c963d47920b2b32079ff
RynRam <ryankenethramiso@gmail.com>
2019-03-16 23:47:19 +0800

    Email

--- config/services.php
+++ config/services.php
@@ -35,7 +35,7 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
     'sendgrid' => [
-        'api_key' => env('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'),
+        'api_key' => env('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'),
     ],
 
 ];
