10d09cceb3b2ac3a4ec5dfa9c3f54824f28c690e
RynRam <ryankenethramiso@gmail.com>
2019-04-29 01:33:26 +0800

    Final/Test User Updated

--- resources/views/admin/usersManagement/index.blade.php
+++ resources/views/admin/usersManagement/index.blade.php
@@ -52,7 +52,7 @@
             </td>
                 </tr>
                         <!-- edit -->
-              <div class="modal fade" id="reserve_edit{{$reservation->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal fade" id="reserve_edit{{$user->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                   <div class="modal-content">
                     <div class="modal-header">
