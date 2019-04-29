c659dec0e1ed0ec8262a0ba225283e322c43d4f8
RynRam <ryankenethramiso@gmail.com>
2019-04-29 08:19:10 +0800

    Final/Test Layout

--- resources/views/layouts/frontLayout/front_header.blade.php
+++ resources/views/layouts/frontLayout/front_header.blade.php
@@ -13,14 +13,14 @@
       <nav id="nav-menu-container">
         <ul class="nav-menu">
           <li><a href="https://lynagailscaters.herokuapp.com">Home</a></li>
-          <li class="menu-has-children"><a>Services</a>
+          <li class="menu-has-children"><a href="">Services</a>
             <ul>
               <li><a href="https://lynagailscaters.herokuapp.com/basic-catering">Basic Catering</a></li>
               <li><a href="https://lynagailscaters.herokuapp.com/debut">Debut</a></li>
               <li><a href="https://lynagailscaters.herokuapp.com/wedding">Wedding</a></li>
             </ul>
           </li>
-          <li class="menu-has-children"><a>Packages</a>
+          <li class="menu-has-children"><a href="">Packages</a>
             <ul>
             <li><a href="https://lynagailscaters.herokuapp.com/cateringA">Catering Package A</a></li>
               <li><a href="https://lynagailscaters.herokuapp.com/cateringB">Catering Package B</a></li>
@@ -35,14 +35,14 @@
           </li>
           <li><a href="https://lynagailscaters.herokuapp.com/blog">Blog</a></li>
     @if (Auth::guard('customer')->guest())
-          <li class="menu-has-children"><a>Account<a>
+          <li class="menu-has-children"><a href="">Account<a>
             <ul>
               <li><a href="https://lynagailscaters.herokuapp.com/customer-register">Register</a></li>
               <li><a href="https://lynagailscaters.herokuapp.com/customer-login">Login</a></li>
             </ul>
           </li>
          @else
-          <li class="menu-has-children"><a><i class="ion-ios-contact"  style="position:relative !important;"></i>  {{ Auth::guard('customer')->user()->fname }} </a>
+          <li class="menu-has-children"><a href=""><i class="ion-ios-contact"  style="position:relative !important;"></i>  {{ Auth::guard('customer')->user()->fname }} </a>
             <ul>
             <li><a href="https://lynagailscaters.herokuapp.com/customer-profile/{{Auth::guard('customer')->user()->id}}/edit">Profile</a></li>
             <li><a href="https://lynagailscaters.herokuapp.com/my-events">My Events</a></li>
