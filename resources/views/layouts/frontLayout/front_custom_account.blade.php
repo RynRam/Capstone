cbce235a6359e7e041e5fade748679aaa5764160
RynRam <ryankenethramiso@gmail.com>
2019-05-01 18:39:13 +0800

    Final/Test Evaluate

--- resources/views/layouts/frontLayout/front_custom_account.blade.php
+++ resources/views/layouts/frontLayout/front_custom_account.blade.php
@@ -110,8 +110,7 @@
   var left, opacity, scale; //fieldset properties which we will animate
   var animating; //flag to prevent quick multi-click glitches
 
-function testing(){
-  $(".next").click(function eventclick(){
+  $(".next").click(function(){
     if(animating) return false;
     animating = true;
     
@@ -148,7 +147,6 @@ function testing(){
       easing: 'easeInOutBack'
     });
   });
-}
 
   $(".previous").click(function(){
     if(animating) return false;
@@ -191,20 +189,60 @@ function testing(){
 </script>
 <script>
  $("#validate").click(function(e){
-
+  e.preventDefault();
   var pass = $('#pass').val();
   var cpass = $('#cpass').val();
   var correct = true;
     if(cpass !== pass){
       swal('Confirm password must be same to your password!', '', 'error');
-      e.preventDefault();
+       correct = false;
     }
     if(correct){
- 
+      
       var element = document.getElementById("validate");
       element.classList.add("next");
-      return correct;
-      testing();
+
+      var current_fs, next_fs, previous_fs; //fieldsets
+      var left, opacity, scale; //fieldset properties which we will animate
+      var animating; //flag to prevent quick multi-click glitches
+
+      $(".next").click(function(){
+        if(animating) return false;
+        animating = true;
+        
+        current_fs = $(this).parent();
+        next_fs = $(this).parent().next();
+        
+        //activate next step on progressbar using the index of next_fs
+        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+        
+        //show the next fieldset
+        next_fs.show(); 
+        //hide the current fieldset with style
+        current_fs.animate({opacity: 0}, {
+          step: function(now, mx) {
+            //as the opacity of current_fs reduces to 0 - stored in "now"
+            //1. scale current_fs down to 80%
+            scale = 1 - (1 - now) * 0.2;
+            //2. bring next_fs from the right(50%)
+            left = (now * 50)+"%";
+            //3. increase opacity of next_fs to 1 as it moves in
+            opacity = 1 - now;
+            current_fs.css({
+              'transform': 'scale('+scale+')',
+              'position': 'relative'
+            });
+            next_fs.css({'left': left, 'opacity': opacity});
+          }, 
+          duration: 800, 
+          complete: function(){
+            current_fs.hide();
+            animating = false;
+          }, 
+          //this comes from the custom easing plugin
+          easing: 'easeInOutBack'
+        });
+      });
 
 
     }
